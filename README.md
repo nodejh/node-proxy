a280c573a8d74c9138fbd79e57bda4efc827f531
nodejh <jianghangscu@gmail.com>
2016-11-27 18:41:37 +0800

    隧道代理

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-## 用 Node.js 实现的反向代理程序
+## 用 Node.js 实现的代理程序
+
+
+
+设置终端代理
+
+```
+export http_proxy=ip:port
+```
+
+测试
+
+
+```
+ab -n 1000 -c 10 http://localhost:9000/
+```
--- proxy.js
+++ proxy.js
@@ -1,10 +1,18 @@
 const http = require('http');
 const url = require('url');
+const net = require('net');
+
 
 const port = process.env.PORT || 9000;
 
 
-const server = http.createServer((req, res) => {
+/**
+ * 普通代理
+ * @param req
+ * @param res
+ */
+const request = (req, res) => {
+  console.log('request...');
   const urlParse = url.parse(req.url);
   const options = {
     host: urlParse.host,
@@ -25,12 +33,33 @@ const server = http.createServer((req, res) => {
   });
 
   req.pipe(proxyReq);
-});
+};
 
 
+/**
+ * 隧道代理
+ * @param req
+ * @param sock
+ */
+const connect = (req, sock) => {
+  console.log('connect...');
+  const urlParse = url.parse(`http://${req.url}`);
+  const proxySock = net.connect(urlParse.port, urlParse.hostname, () => {
+    sock.write('HTTP/1.1 200 Connection Established\r\n\r\n');
+    proxySock.pipe(sock);
+  }).on('error', (e) => {
+    console.log('e: ', e);
+    sock.end();
+  });
+
+  sock.pipe(proxySock);
+};
 
 
-server.listen(port, () => {
+http.createServer()
+  .on('request', request)
+  .on('connect', connect)
+  .listen(port, () => {
   console.log(`Server is running at port ${port}`);
 });
 
